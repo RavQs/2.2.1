edb1aa71082c304e9c90e61870ca1e32ae9a66cf
RavQs <ravenq1112@gmail.com>
2022-02-24 06:47:40 +0900

    Add Car's Dao,Service Add class Car in config add Car's Object in MainApp

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,12 +15,23 @@ public class MainApp {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
+      CarService carService = context.getBean(CarService.class);
+      Car honda = new Car("Honda",222);
+      Car hyundai = new Car("Huindai",333);
+      Car kia = new Car("Kia", 1);
+      Car Toyota = new Car("Toyota", 2);
+      carService.add(honda);
+      carService.add(hyundai);
+      carService.add(kia);
+      carService.add(Toyota);
+
+
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",honda));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",hyundai));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",kia));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",Toyota));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +39,7 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " + user.getCar());
          System.out.println();
       }
 
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -3,7 +3,7 @@ package hiber.model;
 import javax.persistence.*;
 
 @Entity
-
+@Table(name = "cars")
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,6 +12,51 @@ public class Car {
     private String model;
     @Column
     private int series;
+    @OneToOne(mappedBy = "car")
+    private User user;
 
     public Car(){}
+
+    public Car(String model, int series){
+        this.model = model;
+        this.series = series;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                ", user=" + user +
+                '}';
+    }
 }
